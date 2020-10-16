eb32c0d77cb074c53a832221f577bef08c020503
gothmog <husak.ondra@gmail.com>
2020-10-16 18:50:14 +0200

    úprava save

--- ProjectLeader/Classes/History.cs
+++ ProjectLeader/Classes/History.cs
@@ -1,16 +1,19 @@
 ﻿using MongoBaseRepository.Classes;
 using MongoDB.Bson.Serialization.Attributes;
 using ProjectLeader.Classes.Enums;
+using System;
 
 namespace ProjectLeader.Classes
 {
-  public class History: BaseEntity
-  {
-    public PriorityEnum PriorityValuePast { get; set; }
-    public StateEnum StateValuePast { get; set; }
-    public TypeEnum TypeValuePast { get; set; }
-    public string UserIdPast { get; set; }
-    [BsonIgnore]
-    public bool IsChanged { get; set; }
-  }
+    public class History : BaseEntity
+    {
+        public PriorityEnum PriorityValuePast { get; set; }
+        public StateEnum StateValuePast { get; set; }
+        public TypeEnum TypeValuePast { get; set; }
+        public DateTime? StartPast { get; set; }
+        public DateTime? EndPast { get; set; }
+        public string UserIdPast { get; set; }
+        [BsonIgnore]
+        public bool IsChanged { get; set; }
+    }
 }
